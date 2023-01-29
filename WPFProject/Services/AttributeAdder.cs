2f4cadeab5c6b006dc97e56237e20e2cb8acd173
Александр <HellaHoesCity@gmail.com>
2023-01-29 18:43:17 +0700

    feat: добавить сервис по добавлению атрибутов

---
+++ WPFProject/Services/AttributeAdder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+
+namespace WPFProject.Services
+{
+    /// <summary>
+    /// Представляет сервис по добавлению атрибутов.
+    /// </summary>
+    internal class AttributeAdder : XmlTransformer
+    {
+        #region Методы
+
+        /// <summary>
+        /// Добавляет атрибут count для всех элементов, потомками которых являются элементы item.
+        /// </summary>
+        /// <param name="path"> Путь к xml файлу. </param>
+        public void AddAttributeWithNumberOfElements(string path)
+        {
+            var doc = new XmlDocument();
+            doc.Load(path);
+
+            var allNodesForChildCount = GetAllNodesForChildCount(doc);
+
+            foreach(var node in allNodesForChildCount)
+            {
+                int childCount = node.ChildNodes.Count;
+                var xmlElement = (XmlElement)node;
+                xmlElement.SetAttribute("count", childCount.ToString());
+            }
+
+            doc.Save(path);
+        }
+
+        #endregion Методы
+    }
+}
