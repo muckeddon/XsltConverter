a3bc63c426f04c6c818b3e355616bc969433e285
Александр <HellaHoesCity@gmail.com>
2023-01-29 20:31:28 +0700

    feat: добавить перечисление видов путей

---
+++ WPFProject/ViewModels/Base/PathType.cs
@@ -0,0 +1,18 @@
+﻿namespace WPFProject.ViewModels.Base
+{
+    /// <summary>
+    /// Представляет перечисление видов путей.
+    /// </summary>
+    internal enum PathType
+    {
+        /// <summary>
+        /// Путь для сохранения документа.
+        /// </summary>
+        PathForSave,
+
+        /// <summary>
+        /// Путь для открытия документа.
+        /// </summary>
+        PathForOpen
+    }
+}
